750e2853946045efe32057d009d3d9ff220314c2
Facundo <156367881+Facundo223@users.noreply.github.com>
2026-02-08 12:45:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const envelope = document.getElementById("envelope");
-const sorpresa = document.getElementById("sorpresa");
-const tapSorpresa = document.querySelector(".tap-sorpresa");
 
 envelope.addEventListener("click", () => {
   envelope.classList.add("open");
-});
 
-tapSorpresa.addEventListener("click", (e) => {
-  e.stopPropagation();
-  sorpresa.classList.remove("hidden");
-  tapSorpresa.style.display = "none";
-  envelope.classList.add("show-sorpresa");
+  
+  setTimeout(() => {
+    envelope.classList.add("hide");
+  }, 500); 
 });
